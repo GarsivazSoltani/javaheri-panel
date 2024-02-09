6a6509999e5321714c779538fab24a8cb7d0f1ad
GarsivazSoltani <garsi.soltani@gmail.com>
2024-02-09 00:21:44 -0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title' , __('public.main page'))
+
+@section('content')
+    <div class="flex-center course-title position-ref full-height">
+        <div class="content mt-3">
+            {{-- @include('partials.alerts') --}}
+            <div class="title m-b-md">
+                @lang('public.register & login system')
+            </div>
+            <div class="library-title">
+                @lang('public.practical laravel')
+            </div>
+        </div>
+    </div>
+@endsection
